812a680cffd9a7df999a8b89a5e8bd012922f4c8
Kore Nordmann <mail@kore-nordmann.de>
2017-02-20 13:01:03 +0100

    Use parser to return currently played song

--- src/php/SpotifyBundle/Controller/IndexController.php
+++ src/php/SpotifyBundle/Controller/IndexController.php
@@ -10,13 +10,15 @@ use Symfony\Component\Routing\Route;
 use QafooLabs\MVC\TokenContext;
 
 use Kore\Spotify\SpotifyBundle\Controller\Index\Context;
+use Kore\Spotify\SpotifyBundle\Domain\DbusResponseParser;
 
 class IndexController extends Controller
 {
     public function currentSongAction(TokenContext $context)
     {
-        // @TODO: Parse result
-        $result = shell_exec("dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'");
+        $result = shell_exec("dbus-send --print-reply --reply-timeout=100 --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'");
+        $parser = new DbusResponseParser();
+        $result = $parser->parse($result);
 
         return new JsonResponse(
             array(
