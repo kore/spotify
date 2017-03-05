14ef78a0b31ed21e4b648650a5d2b5c18c246e21
Kore Nordmann <mail@kore-nordmann.de>
2017-03-05 17:50:35 +0100

    Siwtched to FlexBox layout

--- src/js/overview.jsx
+++ src/js/overview.jsx
@@ -3,26 +3,35 @@
 import React from "react"
 
 import NowPlaying from './nowPlaying.jsx'
+import PlaylistSelector from './playlist/selector.jsx'
 import SearchablePlaylist from './playlist/searchable.jsx'
 
 let Overview = React.createClass({
     render: function () {
         return (<main className="layout-row">
-            <div className="layout-cell">
-                <h1>Now Playing</h1>
-                <NowPlaying />
-                <h1>Up Next</h1>
-                <ul className="queue list-unstyled">
-                    <li>Track 1</li>
-                </ul>
+            <div className="layout-column">
+                <div className="layout-cell">
+                    <h1>Now Playing</h1>
+                    <NowPlaying />
+                </div>
+                <div className="layout-cell scroll">
+                    <SearchablePlaylist />
+                </div>
             </div>
-            <div className="layout-cell">
-                <SearchablePlaylist />
+            <div className="layout-column">
+                <div className="layout-cell">
+                    <PlaylistSelector />
+                </div>
+                <div className="layout-cell scroll">
+                    <SearchablePlaylist />
+                </div>
             </div>
-            <div className="layout-cell">
-                <h1>Search</h1>
-                <ul className="playlist list-unstyled">
-                </ul>
+            <div className="layout-column">
+                <div className="layout-cell">
+                    <h1>Search</h1>
+                    <ul className="playlist list-unstyled">
+                    </ul>
+                </div>
             </div>
         </main>)
     },
